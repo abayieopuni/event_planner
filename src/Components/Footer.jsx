6824ce9211cfe44c309dcc55736091a97c38f8a6
Kwame Opuni <abayieremi@gmail.com>
2025-01-08 01:16:15 -0500

    adding completed lab

---
+++ src/Components/Footer.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import './EventPlanner.css';
+
+
+export default function Footer (){
+    return (
+        <>
+            <footer>
+                <p>&copy. Event Planner Organization. All rights reserved</p>
+            </footer>
+        </>
+       
+    );
+}
